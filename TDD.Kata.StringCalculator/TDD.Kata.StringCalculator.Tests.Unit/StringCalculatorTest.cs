0ef744b474d9b7bff6726b6bf6c444a5daa5c740
Facu <Facu@MSI>
2017-08-30 10:25:21 -0300

    Agrego StringCalculatorTest

---
+++ TDD.Kata.StringCalculator/TDD.Kata.StringCalculator.Tests.Unit/StringCalculatorTest.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+namespace TDD.Kata.StringCalculator.Tests.Unit
+{
+   
+    public class StringCalculatorTest
+    {
+        public StringCalculator Target;
+
+        public StringCalculatorTest()
+        {
+            this.Target = new StringCalculator();
+        }
+
+        [Fact]
+        public void debe_devolver_un_cero()
+        {
+            //Arrange
+            string numbers = string.Empty;
+
+            //Act
+           int result = Target.add(numbers);
+
+            //assert
+            Assert.True(0==result);
+        }
+    }
+}
---
+++ TDD.Kata.StringCalculator/TDD.Kata.StringCalculator/StringCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD.Kata.StringCalculator
+{
+   public class StringCalculator
+    {
+        public int add(string numbers)
+        {
+            return default(int);
+        }
+    }
+}
