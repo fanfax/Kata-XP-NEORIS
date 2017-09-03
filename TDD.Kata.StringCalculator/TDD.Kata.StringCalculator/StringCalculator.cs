ab01a89781e1857fa11a5bed0746c2d0b2c346e6
Facu <Facu@MSI>
2017-09-03 16:53:13 -0300

    Agrago el separador \n

--- TDD.Kata.StringCalculator/TDD.Kata.StringCalculator/StringCalculator.cs
+++ TDD.Kata.StringCalculator/TDD.Kata.StringCalculator/StringCalculator.cs
@@ -15,30 +15,11 @@ namespace TDD.Kata.StringCalculator
                 return default(int);
             }
 
-            List<string> stringList = new List<string>();
-            int number;
-            string numberString = string.Empty;
-
-            //Separo los numeros del string
-            numbers = numbers.Trim();
-            foreach (char c in numbers)
-            {
-                try
-                {
-                    number = int.Parse(c.ToString());
-                    numberString += number;
-                }
-                catch
-                {
-                    stringList.Add(numberString);
-                    numberString = string.Empty;
-                }
-            }
-            if (!string.IsNullOrEmpty(numberString)) stringList.Add(numberString);
-
-            //Totaliza
             int totalizer = 0;
-            foreach (String stringNumber in stringList)
+            String[] arrayString = numbers.Split(',','\n');
+            int number;
+
+            foreach (String stringNumber in arrayString)
             {
                 number = 0;
                 int.TryParse(stringNumber, out number);
